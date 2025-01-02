201d78ce1fac567fe1fe6cf5ea094a3a7dd69978
Mahiro <work.mahiro@outlook.com>
2025-01-02 13:39:22 -0800

    second commit

--- src/components/Ps1.svelte
+++ src/components/Ps1.svelte
@@ -6,6 +6,6 @@
 <h1 class="font-bold flex">
   <span style={`color: ${$theme.yellow};`}>titty</span>
   <span style={`color: ${$theme.white}`}>@</span>
-  <span style={`color: ${$theme.green}`}>titty-lover</span>
+  <span style={`color: ${$theme.green}`}>tittylover</span>
   <span style={`color: ${$theme.white}`}>:~$</span>
 </h1>
